3bf539867266b0827241a3c7acf6fb40ea1fb499
sdwan <hebinbin@netbank.cn>
2020-05-07 19:36:59 +0800

    change upgrade_image method

--- Cpe_Cli.py
+++ Cpe_Cli.py
@@ -1,3 +1,4 @@
+import os
 import re,time
 import paramiko
 import subprocess
@@ -6,71 +7,84 @@ from SSHLibrary.library import SSHLibrary
 
 class Cpe_Cli(SSHLibrary):
     
-    def _get_latest_image(self):
-        client=paramiko.SSHClient()
-        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        client.connect(hostname='172.31.25.77',username='autobuild',password='linkwan123456')
-        stdin,stdout,stderr=client.exec_command('ls -t /home/developers/images')
+    def _get_latest_image(self,image_type):
+        
         try:
-            output=stdout.read().decode('utf-8')
-            all_image=output.strip().split('\n')
-            latest_image=all_image[0]
+            client = paramiko.SSHClient()
+            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            client.connect(hostname='172.31.25.77',username='autobuild',password='linkwan123456')
+            stdout = client.exec_command('ls -t /home/developers/images/%s' % image_type)        
+            output = stdout[1].read().decode('utf-8')
+            all_image_dir = output.split('\n')
+            client.close()
+
+            latest_image = os.path.split(all_image_dir[0])[1]
             logger.info('latest_image is: %s' % latest_image)
+        
         except Exception as e:
             latest_image='no image found!'
             logger.warn(e)
-        client.close()
         return latest_image
 
 
-    def upgrade_image(self,url='default',clean_conf='false',timeout=300):
+    def upgrade_image(self,image_name,clean_conf='false'):
         """Upgrade image for CPE via using command 'sysupgrade (-n) url'
         if you want to upgrade CPE with the latest version of image for example:
             | open connection | ${cpe_ip} |
-            | login | ${cpe_username} | ${cpe_password} |
-            | upgrade image |
+            | login | ${cpe_username} | ${cpe_password} | 
+            | upgrade image | E201 |
 
         if you want to use the specific version of image, replace the part of upgrade image to:
-            | upgrade image | http://172.31.25.77:8880/images/CPE-201-image-XXXXXX.bin |
+            | upgrade image | E201-vx.x.x.bin |
 
         if you want to clean the config after upgrade CPE, append argument clean_conf=true as below:
             | upgrade image | clean_conf=true |
             or:
-            | upgrade image | http://172.31.25.77:8880/images/CPE-201-image-XXXXXX.bin | clean_conf=true |
+            | upgrade image | E201-vx.x.x.bin | clean_conf=true |
         """
-        if url=='default':
-            image=self._get_latest_image()
+        if image_name == 'E201' or image_name == 'V501':
+            image = self._get_latest_image(image_name)
             if image=='no image found!':
                 logger.warn('no image found,please check whether image exists in file path http://172.31.25.77:8880/images')
                 raise AssertionError('no image found')
-            url='http://172.31.25.77:8880/images/'+image
-            logger.info('image_url is: %s' % url)
+        elif image_name.endswith('bin') or image_name.endswith('img'):
+            image = image_name    
+
+        url='http://172.31.25.77:8880/images/'+image
+        logger.info('image_url is: %s' % url)
+
+        self._upgrade_image(url,clean_conf)
         
-        if clean_conf=='false':
-            cmd='sysupgrade '+url
-        if clean_conf.lower()=='true':
-            cmd='sysupgrade -n '+url
-        logger.info('upgrde CPE via command: %s' % cmd)
-        current_host=self.current.config.host
+
+    def _upgrade_image(self,url,clean_conf):
+
+        if clean_conf == 'false':
+            cmd = 'sysupgrade ' + url
+        elif clean_conf.lower() == 'true':
+            cmd = 'sysupgrade -n ' + url
+        logger.info('upgrade CPE via command: %s' % cmd,also_console=True)
         try:
-            output=self.current.execute_command(cmd)
-            logger.info(output[1])
-            logger.info(output[0])
-            upgrade_result=output[1]
+            current_host = self.current.config.host
+            output = self.current.execute_command(cmd)
+            upgrade_result = output[1]
+            logger.info(upgrade_result,also_console=True)
+            logger.info(output[0],also_console=True)
             if not re.search(r'Download completed',upgrade_result):
-                logger.warn('Download from %s failed' % url)
+                logger.warn('Download image from %s failed!' % url)
             if re.search(r'Closing all shell sessions',output[0]):
                 time.sleep(15)
-                cpe_connection=self._ping_device(current_host)
-                if cpe_connection==1:
-                    logger.info('ping cpe success, checking image version of cpe...')
-                    self.open_connection(current_host)
+                cpe_connection = self._ping_device(current_host)
+                if cpe_connection == 1:
+                    logger.info('ping cpe success,prepare to check current image version of cpe')
+                    #self.open_connection(current_host)
                     #open connection but not login due to lack of usr/pwd
-                if cpe_connection==0:
-                    logger.warn('ping cpe timeout')
-
+                if cpe_connection == 0:
+                    logger.warn('ping cpe timeout,please check cpe status manually')
         except Exception as e:
-            logger.warn(e)
+            logger.error(e)
+    
+
+
 
     def _ping_device(self,host,total=30):
         logger.info('ping device with host ip: %s' % host)
